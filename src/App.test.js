eaae5d7398bb9ab46dffd246bce6d10c6be37daa
Federico Aloi <federico.aloi@gmail.com>
2020-12-17 18:42:52 -0300

    Arreglado test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renderiza el título', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Repositorio semilla/);
   expect(linkElement).toBeInTheDocument();
 });
